b6e7c0e28a38e9f3e206f316aa473d0983462c43
Alfred Nithin <alfrednithin@144.121.140.150.lightower.net>
2019-11-20 23:22:20 -0500

    Alfred_INFO600_Assignment3

--- js/main.js
+++ js/main.js
@@ -1,26 +1,98 @@
-document.addEventListener('DOMContentLoaded', assignClickHandler)
+  $(document).ready(function(){
+    $("#loadData").click(function(){
+    $("#showData").empty();
+    $.getJSON("/users", function(result){
+      $.each(result, function(i, field){
 
-function assignClickHandler () {
-  document.getElementById('addRec').addEventListener('click', function () {
-    const startYear = document.getElementById('startYear').value
-    if (startYear <= 2000) {
+       for(var j=0; j<field.length; j++){
+
+        $("#showData").append(field[j].fullName + " , " + field[j].major + " ,  " + field[j].startYear + "<button value='" + field[j].id + "' id ='deleteData'>Delete</button>"  + "<br>" );
+        console.log(field[j].id);
+        
+         }
+      
+      });
+    });
+    });
+  });
+
+
+
+  $(document).ready(function(){
+    $("#addRec").click(function(){
+    const startYear= document.getElementById('startYear').value;
+    if (startYear <= 2000) 
+
+      {
       window.alert('Incorrect year: ' + startYear)
       return
+      }
+
+    $.ajax ({
+
+        method:'POST',
+        url:'/users/',
+        type:'POST',
+        data: {
+
+          fullName:$('#fullName').val(),
+          major:$('#major').val(),
+          startYear: $('#startYear').val(),
+          
+        }
+        
+        
+      });
+
+      document.getElementById('inputs').reset();
+
+      });
+      });
+      
+      
+      
+      
+  $(document).on("click","#deleteData",function(){
+
+     const id= $(this).val(); 
+    console.log(id);
+    $.ajax({
+
+        method:'DELETE',
+        url:'/user/'+id,
+        type:'',
+        
+        cache: false,
+        
+          
+        }
+        
+        
+      )
+      
+      onLoad();
+
+      });
+      
+      
+      
+    
+function onLoad(){
+
+      document.getElementById("showData").innerHTML = " ";
+      $.getJSON("/users", function(result){
+      $.each(result, function(i, field){
+       for(var j=0; j<field.length; j++){
+
+        $("#showData").append(field[j].fullName + " , " + field[j].major + " ,  " + field[j].startYear + "<button value='" + field[j].id + "' id ='deleteData'>Delete</button>" + "<br>" );
+        console.log(field[j].id);
+
+         }
+      
+      });
+    });
     }
-    const fullName = document.getElementById('fullName').value
-    const major = document.getElementById('major').value
-
-    const date = new Date()
-    const time = date.getHours() + ':' + date.getMinutes()
-
-    const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
-
-    const enteredRecords = document.getElementById('enteredRecords')
-    let newChild = document.createElement('li')
-    newChild.appendChild(document.createTextNode(newEntry))
-
-    enteredRecords.appendChild(newChild)
-
-    document.getElementById('inputs').reset()
-  })
-}
+         
+      
+      
+  
